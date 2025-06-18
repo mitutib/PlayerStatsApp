0c4a75c1b43972318b75a869c9361173b348447c
Mitu Tiberiu <mitutiberiu9@gmsil.com>
2025-06-18 11:51:42 +0300

    Integration test

--- PlayerStatsApp-20250116T130828Z-001/PlayerStatsApp/PlayerStats/demo/src/test/java/com/example/demo/controller/PlayerControllerIntegrationTest.java
+++ PlayerStatsApp-20250116T130828Z-001/PlayerStatsApp/PlayerStats/demo/src/test/java/com/example/demo/controller/PlayerControllerIntegrationTest.java
@@ -85,6 +85,7 @@ public class PlayerControllerIntegrationTest {
     }
 
     @Test
+    @DisplayName("Player been successfully deleted")
     void testDeletePlayer() throws Exception {
 
         String playerJson = """
