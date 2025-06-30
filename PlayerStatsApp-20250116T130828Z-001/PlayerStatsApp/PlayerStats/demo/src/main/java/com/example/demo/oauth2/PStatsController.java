c9cddba5360aaf44433fd7df6ed7f99e68384581
Mitu Tiberiu <mitutiberiu9@gmsil.com>
2025-06-30 12:11:53 +0300

    Google Security Login

---
+++ PlayerStatsApp-20250116T130828Z-001/PlayerStatsApp/PlayerStats/demo/src/main/java/com/example/demo/oauth2/PStatsController.java
@@ -0,0 +1,17 @@
+package com.example.demo.oauth2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PStatsController {
+
+@GetMapping("/")
+    public String hello(){
+
+        return "Welcome to PlayerStats";
+
+
+    }
+
+}
---
+++ PlayerStatsApp-20250116T130828Z-001/PlayerStatsApp/PlayerStats/demo/src/main/java/com/example/demo/oauth2/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.example.demo.oauth2;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+@Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/", "/login/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth2 -> oauth2
+                        .defaultSuccessUrl("/oauth2/success", true)
+                );
+
+
+
+
+        return http.build();
+    }
+
+
+}
