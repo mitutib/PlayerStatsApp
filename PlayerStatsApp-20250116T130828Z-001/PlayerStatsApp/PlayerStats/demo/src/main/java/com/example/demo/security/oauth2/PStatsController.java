d57b63b8353776ce2173a24640348944b518885f
Mitu Tiberiu <mitutiberiu9@gmsil.com>
2025-07-01 11:23:18 +0300

    Google Security Login

--- PlayerStatsApp-20250116T130828Z-001/PlayerStatsApp/PlayerStats/demo/src/main/java/com/example/demo/security/oauth2/PStatsController.java
+++ PlayerStatsApp-20250116T130828Z-001/PlayerStatsApp/PlayerStats/demo/src/main/java/com/example/demo/security/oauth2/PStatsController.java
@@ -1,4 +1,4 @@
-package com.example.demo.oauth2;
+package com.example.demo.security.oauth2;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
--- PlayerStatsApp-20250116T130828Z-001/PlayerStatsApp/PlayerStats/demo/src/main/java/com/example/demo/security/oauth2/SecurityConfig.java
+++ PlayerStatsApp-20250116T130828Z-001/PlayerStatsApp/PlayerStats/demo/src/main/java/com/example/demo/security/oauth2/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.example.demo.oauth2;
+package com.example.demo.security.oauth2;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
